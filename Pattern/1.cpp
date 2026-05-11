7ad3d3a68a18ec570793252731549e8a8c0585fb
Pruthvi Patel <pruthvipatel889@gmail.com>
2026-05-11 22:54:23 +0530

    update1.cpp

--- Pattern/1.cpp
+++ Pattern/1.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+void printPattern(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=i;j++){
             cout << i;
         }
         cout << endl;
     }
+}
+int main(){
+    printPattern();
 }
