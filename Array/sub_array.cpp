e317afbe1d4c50dec0953148869ff32ac6aa4367
Pruthvi Patel <pruthvipatel889@gmail.com>
2026-05-13 18:49:10 +0530

    Sub Array

---
+++ Array/sub_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void subArray(int *arr,int n){
+    for(int st=0;st<n;st++){
+        for(int end=st;end<n;end++){
+            for(int i=st;i<=end;i++){
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr) / sizeof(int);
+    subArray(arr,n);
+}
